ac47983234c536492b37f1188a634ab9f85421d1
singletonc <charles.singleton@hitachivantara.com>
2024-04-23 16:12:17 -0400

    [BACKLOG-40086] Schedule Output Perspective : Allow user to browse Scheduled contents from a VFS location folder Follow up sub-task: [BACKLOG-40485] Disable file action buttons for multi-select, and disable double-click to "open" for VFS Connections - These blocks/code will be modified in BACKLOG-40475 wherein we investigate enabling "open" for VFS Connection files [BACKLOG-40486] Investigate ctrl+click in VFS Connection Files (cannot ctrl+click to add >1 to selection, only remove) - Add `id` to file elements in cases wher `objectId` is missing in response object, use `path` instead + Ensure full URL encoding for tree requests

--- user-console/src/main/resources/org/pentaho/mantle/public/browser/js/browser.js
+++ user-console/src/main/resources/org/pentaho/mantle/public/browser/js/browser.js
@@ -450,7 +450,6 @@ define([
 
       var filePath = clickedFile.obj.attr("path");
       const isRepoPath = isRepositoryPath(filePath);
-      fileButtons.enableButtons(isRepoPath);
 
       //Ajax request to check write permissions for file
       if( isRepoPath ) {
@@ -703,6 +702,11 @@ define([
 
           obj.file = response.children[i].file;
           obj.file.pathText = jQuery.i18n.prop('originText') + " " //i18n
+
+          if(!obj.file.objectId){
+            obj.file.objectId = obj.file.path;
+          }
+
           newResp.children.push(obj);
         }
       }
@@ -1010,6 +1014,14 @@ define([
           }
         });
       });
+
+      // TODO BACKLOG-40086: for now, disable all file actions for VFS Connections, i.e. isRepoPath = false
+      const selectedFilePath = fileClicked ? fileClicked.attr("path") : null;
+      if (selectedFilePath != null && buttonsType.enableButtons) {
+        const isRepoPath = isRepositoryPath(selectedFilePath);
+        buttonsType.enableButtons(isRepoPath);
+      }
+
       model.updateFolderButtons( folderClicked == undefined ? window.parent.HOME_FOLDER : folderClicked.attr("path") );
     },
 
@@ -1067,6 +1079,13 @@ define([
           }
         });
       });
+
+      // TODO BACKLOG-40086: for now, disable all file actions for VFS Connections, i.e. isRepoPath = false
+      const selectedFilePath = fileClicked ? fileClicked.attr("path") : null;
+      if (selectedFilePath != null && buttonsType.enableButtons) {
+        const isRepoPath = isRepositoryPath(selectedFilePath);
+        buttonsType.enableButtons(isRepoPath);
+      }
     },
 
     checkButtonsEnabled: function () {
@@ -1864,7 +1883,9 @@ define([
     doubleClickFile: function (event) {
       var path = $(event.currentTarget).attr("path");
       //if not trash item, try to open the file.
-      if (FileBrowser.fileBrowserModel.getLastClick() != "trashItem") {
+
+      //TODO BACKLOG-40086: disable double click actions on VFS Connection files for now. To be addressed in BACKLOG-40475
+      if (isRepositoryRootPath(path) && FileBrowser.fileBrowserModel.getLastClick() != "trashItem") {
         this.model.get("openFileHandler")(path, "run");
       }
     },
